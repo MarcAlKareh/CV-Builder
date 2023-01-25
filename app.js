46c367eae1325b547a75ee8dc81068bc50104f1d
MarcAlKareh <ma7748@student.st-chris.net>
2023-01-25 16:51:14 +1100

    basic functionality for first section of form

---
+++ app.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const data = {};
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+  res.redirect('/contact-details');
+});
+
+app.get('/contact-details', (req, res, next) => {
+  res.render('contact-details', {
+    pageTitle: 'Contact Info',
+  });
+});
+
+app.post('/contact-details', (req, res, next) => {
+  const body = req.body;
+
+  data.fname = body.fname;
+  data.lname = body.lname;
+  data.phone = body.phone;
+  data.email = body.email;
+  data.address = body.address;
+
+  res.redirect('/experience');
+});
+
+app.listen(3000);
