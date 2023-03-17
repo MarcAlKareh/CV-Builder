e09909dced965cba2052eba0519121bd9fd63837
Fleskimiso <dawid82228@o2.pl>
2023-03-17 16:55:53 +0100

    init changes

--- app.js
+++ app.js
@@ -160,4 +160,8 @@ app.get('/final-resume', (req, res, next) => {
   });
 });
 
-app.listen(3000);
+const port = process.env.PORT || 3000
+
+app.listen(port, ()  =>{
+  console.log("listening on port", 3000);
+});
