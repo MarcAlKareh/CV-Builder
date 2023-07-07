647f1157eb1b6d9fa4bcaa52341965444d52d4e6
Vir Toolsidass <vir.toolsidass@gmail.com>
2023-07-07 18:55:31 +0400

    added error handling

--- public/js/summary-page.js
+++ public/js/summary-page.js
@@ -2,12 +2,16 @@ const summaryCheck = document.querySelector('.summary-check');
 
 summaryCheck.addEventListener('keydown', async e => {
     if (e.key === 'Enter') {
-        const res = await fetch(`/summary-ai?input=${summaryCheck.value}`, { method: 'GET' });
-        const { reply } = await res.json();
-        summaryCheck.value = `
-        ${summaryCheck.value}
-            
-        ${reply}
-        `;
+        try {
+            const res = await fetch(`/summary-ai?input=${summaryCheck.value}`, { method: 'GET' });
+            const { reply } = await res.json();
+            summaryCheck.value = `
+            ${summaryCheck.value}
+                
+            ${reply}
+            `;
+        } catch (err) {
+            console.log(err.message);
+        }
     }
 });
