a951fcf89c364535b6aab6943d4547e65ad772f5
MarcAlKareh <ma7748@student.st-chris.net>
2023-10-09 20:29:33 +0400

    added prompts for user to use for AI

--- public/js/ai.js
+++ public/js/ai.js
@@ -6,9 +6,26 @@ const closeIcon = document.querySelector('.close-icon');
 summaryCheck.addEventListener('keydown', async e => {
   if (e.key === 'Enter') {
     try {
-      const res = await fetch(`/summary-ai?input=${summaryCheck.value}`, {
-        method: 'GET',
-      });
+      const currentUrl = window.location.pathname;
+      let q = '';
+
+      if (currentUrl === '/education') {
+        q =
+          'I am writing a CV and I need you to write a description for the education section using this information in 2-3 sentences';
+      } else if (currentUrl === '/summary') {
+        q =
+          'I am writing a professional summary for my CV. Here are some of the details you need to write me a professional summary and please make it 4 lines long';
+      } else {
+        q =
+          'I am writing a CV and I need three bullet points as part of my responsibilities during a position. Here are three main tasks I did and the job';
+      }
+
+      const res = await fetch(
+        `/summary-ai?input=${summaryCheck.value}&q=${q}`,
+        {
+          method: 'GET',
+        }
+      );
       const { reply } = await res.json();
       summaryCheck.value = `
             ${summaryCheck.value}
